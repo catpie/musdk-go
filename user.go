fcdfe72acabcaeaf4f41ab04d2d9a8142f5b0de9
orvice <orvice@gmail.com>
2016-10-28 00:57:25 +0800

    update user

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package musdk
 
-import ()
+import (
+	ss "github.com/orvice/shadowsocks-go/shadowsocks"
+	"strings"
+)
 
 const (
 	StatusEnable  = 1
@@ -16,4 +19,38 @@ type User struct {
 	TransferEnable int64  `json:"transfer_enable"`
 	U              int64  `json:"u"`
 	D              int64  `json:"d"`
+}
+
+func (u User) GetPort() int {
+	return u.Port
+}
+
+func (u User) GetPasswd() string {
+	return u.Passwd
+}
+
+func (u User) GetMethod() string {
+	return u.Method
+}
+
+func (u User) IsEnable() bool {
+	if u.Enable == 0 {
+		return false
+	}
+	if u.TransferEnable < (u.U + u.D) {
+		return false
+	}
+	return true
+}
+
+func (u User) GetCipher() (*ss.Cipher, error, bool) {
+	method := u.Method
+	auth := false
+
+	if strings.HasSuffix(method, "-auth") {
+		method = method[:len(method)-5]
+		auth = true
+	}
+	s, e := ss.NewCipher(method, u.Passwd)
+	return s, e, auth
 }
